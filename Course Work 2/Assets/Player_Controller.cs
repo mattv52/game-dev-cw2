9fb34efadde51ee67385fd170a88d2a739d06ae4
dan-sw1 <100227618+dan-sw1@users.noreply.github.com>
2022-12-14 11:32:50 +0000

    Updated game
    Updated game

---
+++ Course Work 2/Assets/Player_Controller.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    private float moveSpeed = 1.0f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
+    Rigidbody2D rb;
+
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    Vector2 movementInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            print(movementInput);
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+            }
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(0, movementInput.y));
+            }
+
+        }
+
+    }
+
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            // Check for potential collisions
+            int count = rb.Cast(
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            // Can't move if there's no direction to move in
+            return false;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+}
