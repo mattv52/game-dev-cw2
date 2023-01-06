974e6ef9a91b05dd57e3e1329e196976a74fe2e7
mattv52 <matthewvolaris@yahoo.com.au>
2023-01-06 00:06:07 +0000

    Started adding talk to npc

--- Course Work 2/Assets/Scripts/NPC_Core.cs
+++ Course Work 2/Assets/Scripts/NPC_Core.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NPC_Core : NPC_Basic
 {
+    public GameObject panel;
+    public GameObject grid;
+    public GameObject template;
+    public string[] replies;
+
+    void Start() 
+    {
+        panel.SetActive(false);
+    }
+
     public void Talk()
     {
+        panel.SetActive(true);
         
+        foreach (string reply in replies)
+        {
+            GameObject temp = Instantiate(template, new Vector3(0, 0, 0), Quaternion.identity);
+            temp.transform.SetParent(grid.transform);
+            temp.transform.GetChild(0).GetComponent<TMP_Text>().text = reply;
+        }
+
     }
-    // Talk
+
     // Give Item
 }
