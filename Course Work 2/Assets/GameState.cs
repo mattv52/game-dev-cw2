a01664b3e08ebc1389d1a69d5d380326c624b697
dan-sw1 <100227618+dan-sw1@users.noreply.github.com>
2023-01-08 17:07:45 +0000

    updates

---
+++ Course Work 2/Assets/GameState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public static bool cell_Mate_Trade_Shive = false;
+    public static bool warden_Wants_More_Proof = false;
+    public static bool give_Cigaret_To_Cook = false;
+    public static bool give_Cigaret_To_Informant = false;
+    public static bool give_Cigaret_To_Guard = false;
+
+    public static bool attack_cellmate = false;
+    public static bool kill_cellmate = false;
+
+    public static bool attack_cook = false;
+    public static bool kill_cook = false;
+
+    public static bool attack_informant = false;
+    public static bool kill_informant = false;
+
+    public static bool attack_guard = false;
+    public static bool kill_guard = false;
+
+    public static bool attack_warden = false;
+    public static bool kill_warden = false;
+
+    public static bool attack_gang_leader = false;
+    public static bool kill_gang_leader = false;
+
+    public static bool attack_gang_lackey = false;
+    public static bool kill_gang_lackey = false;
+
+    
+}
