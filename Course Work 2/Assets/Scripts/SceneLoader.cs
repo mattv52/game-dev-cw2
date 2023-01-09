c8fc1ec2daf0266d7eb0f724ceb309ab4ccde335
mattv52 <matthewvolaris@yahoo.com.au>
2023-01-09 11:20:27 +0000

    Added title screen

--- Course Work 2/Assets/Scripts/SceneLoader.cs
+++ Course Work 2/Assets/Scripts/SceneLoader.cs
@@ -10,33 +10,33 @@ public class SceneLoader : MonoBehaviour
         Application.Quit();
     }
 
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene(0);
-    }
+    // public void LoadMenu()
+    // {
+    //     SceneManager.LoadScene(0);
+    // }
 
-    public void LoadIntro()
-    {
-        SceneManager.LoadScene(1);
-    }
+    // public void LoadIntro()
+    // {
+    //     SceneManager.LoadScene(1);
+    // }
 
     public void LoadMorning()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene("Morning");
     }
 
-    public void LoadMidday()
-    {
-        SceneManager.LoadScene(2);
-    }
+    // public void LoadMidday()
+    // {
+    //     SceneManager.LoadScene(2);
+    // }
 
-    public void LoadAfternoon()
-    {
-        SceneManager.LoadScene(3);
-    }
+    // public void LoadAfternoon()
+    // {
+    //     SceneManager.LoadScene(3);
+    // }
 
-    public void LoadNignt()
-    {
-        SceneManager.LoadScene(4);
-    }
+    // public void LoadNignt()
+    // {
+    //     SceneManager.LoadScene(4);
+    // }
 }
