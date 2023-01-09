508b4e8961eda15b6c8f4dc789c0fbdd09a12a72
mattv52 <matthewvolaris@yahoo.com.au>
2023-01-09 16:29:27 +0000

    Work in progress

--- Course Work 2/Assets/Scripts/GameState.cs
+++ Course Work 2/Assets/Scripts/GameState.cs
@@ -4,48 +4,50 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    public static bool cell_Mate_Trade_Shive = false;
-    public static bool warden_Wants_More_Proof = false;
-    public static bool give_Cigaret_To_Cook = false;
-    public static bool give_Cigaret_To_Informant = false;
-    public static bool give_Cigaret_To_Guard = false;
-    public static bool accept_gang_job = false;
+    public bool cell_Mate_Trade_Shive = false;
+    public bool warden_Wants_More_Proof = false;
+    public bool give_Cigaret_To_Cook = false;
+    public bool give_Cigaret_To_Informant = false;
+    public bool give_Cigaret_To_Guard = false;
+    public bool accept_gang_job = false;
 
-    public static bool gang_break_out = false;
+    public bool gang_break_out = false;
 
-    public static bool cell_Mate_Distract_Guard = false;
-    public static bool getDisc = false;
+    public bool cell_Mate_Distract_Guard = false;
+    public bool getDisc = false;
 
-    public static string job = "1";
+    public string job = "1";
 
     //endings
-    public static bool rat = false;
-    public static bool killAttackerUnNoticed = false;
-    public static bool killAttackerNoticed = false;
-    public static bool escaped = false;
+    public bool rat = false;
+    public bool killAttackerUnNoticed = false;
+    public bool killAttackerNoticed = false;
+    public bool escaped = false;
     //
-    public static bool attack_cellmate = false;
-    public static bool kill_cellmate = false;
+    public bool attack_cellmate = false;
+    public bool kill_cellmate = false;
     //
-    public static bool attack_cook = false;
-    public static bool kill_cook = false;
+    public bool attack_cook = false;
+    public bool kill_cook = false;
     //
-    public static bool attack_informant = false;
-    public static bool kill_informant = false;
+    public bool attack_informant = false;
+    public bool kill_informant = false;
     
-    public static bool attack_guard = false;
-    public static bool kill_guard = false;
+    public bool attack_guard = false;
+    public bool kill_guard = false;
     //
-    public static bool attack_warden = false;
-    public static bool kill_warden = false;
+    public bool attack_warden = false;
+    public bool kill_warden = false;
     //
-    public static bool attack_gang_leader = false;
-    public static bool kill_gang_leader = false;
+    public bool attack_gang_leader = false;
+    public bool kill_gang_leader = false;
     //
-    public static bool attack_gang_lackey = false;
-    public static bool kill_gang_lackey = false;
+    public bool attack_gang_lackey = false;
+    public bool kill_gang_lackey = false;
 
-    public static void reset()
+    
+
+    public void reset()
     {
         cell_Mate_Trade_Shive = false;
         warden_Wants_More_Proof = false;
