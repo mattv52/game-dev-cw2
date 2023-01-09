098bf4c36d6c162b2baaefe440486ed2c4d4bfd2
dan-sw1 <100227618+dan-sw1@users.noreply.github.com>
2023-01-09 18:02:57 +0000

    Update End scene

--- Course Work 2/Assets/Scripts/ending.cs
+++ Course Work 2/Assets/Scripts/ending.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ending : MonoBehaviour
 {
@@ -12,28 +13,42 @@ public class ending : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject inventory = GameObject.FindGameObjectWithTag("UI");
+
+        player.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        inventory.SetActive(false);
+
         gs = GameState.Instance;
 
         if (gs.escaped)
         {
-            endingText.SetText("You escaped, and find out the gang was going to kill you and use you as a distraction when attacking, but your investigation caused enough attention to work");
+            endingText.SetText("You escaped, and find out the gang was going to<br>kill you and use you as a distraction when attacking<br><br>but your investigation caused enough attention to work<br>they decided to leave you in your freedom");
         }
         else if (gs.killAttackerUnNoticed)
         {
-            endingText.SetText("You killed the attacker, and it seemed no one else noticed");
+            endingText.SetText("You killed the attacker<br>and it seemed no one else noticed");
         }
         else if (gs.killAttackerNoticed || gs.kill_gang_lackey)
         {
-            endingText.SetText("You killed the attacker, but the guards noticed, and now youve got a life sentance");
+            endingText.SetText("You killed the attacker<br>but the guards noticed<br>and now youve got a life sentance");
         }
         else if (gs.rat)
         {
-            endingText.SetText("The attacker has been delt with, but now the rest of the prison is wary of you");
+            endingText.SetText("The attacker has been delt with,<br>but now the rest of the prison is wary of you");
         }
         else 
         {
-            endingText.SetText("You didnt deal with the killed");
+            endingText.SetText("You didnt deal with the killer<br>and were found dead the next day");
         }
+    }
+
+    public void reset()
+    {
+        gs.reset();
+        // player.SetActive(false);
+        // UI.SetActive(false);
+        SceneManager.LoadScene(0);
     }
 
 }
